afa35483f148c1b0f1209c32a5d80cf23fca88d5
RajaduraiAz <arajadurai1@live.com>
2024-10-26 00:41:39 +0530

    Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Projects />
       <Contact />
+      <Analytics />
     </div>
   );
 }

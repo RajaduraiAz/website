1c4994067ca9dae9bc2251bc3e505b3b18e0ec0f
RajaduraiAz <arajadurai1@live.com>
2024-10-26 00:46:45 +0530

    SpeedInsights

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Projects />
       <Contact />
       <Analytics />
+      <SpeedInsights />
     </div>
   );
 }
